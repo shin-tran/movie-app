9a43a0544fd63baf4d3975fd7798b04a049daef4
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-04-26 22:10:45 +0700

    feat: implement dynamic tab navigation for media types in MediaList component

--- src/components/MediaList/index.jsx
+++ src/components/MediaList/index.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
+const TABS = [
+  { id: "all", name: "All" },
+  { id: "movie", name: "Movies" },
+  { id: "tv", name: "TV Show" },
+];
+
 const MediaList = () => {
   const [mediaList, setMediaList] = useState([]);
+  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
 
   useEffect(() => {
-    const url = "https://api.themoviedb.org/3/trending/all/day";
+    const url = `https://api.themoviedb.org/3/trending/${activeTabId}/day`;
     const options = {
       method: "GET",
       headers: {
@@ -21,18 +28,26 @@ const MediaList = () => {
         setMediaList(trendingMediaList);
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [activeTabId]);
 
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
         <p className="text-[2vw] font-bold">Trending</p>
         <ul className="flex rounded border border-white px-3 py-1">
-          <li className="cursor-pointer rounded bg-white px-2 py-1 text-black">
-            All
-          </li>
-          <li className="cursor-pointer rounded px-2 py-1">Movie</li>
-          <li className="cursor-pointer rounded px-2 py-1">TV Show</li>
+          {TABS.map((tab) => {
+            return (
+              <li
+                key={tab.id}
+                className={`cursor-pointer rounded ${tab.id === activeTabId ? "bg-white text-black" : ""} px-2 py-1`}
+                onClick={() => {
+                  setActiveTabId(tab.id);
+                }}
+              >
+                {tab.name}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6">

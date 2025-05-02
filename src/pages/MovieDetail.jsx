81f84595c188407325aa7aa34366a9b4f129e78d
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-05-02 09:46:00 +0700

    feat: enhance MovieDetail component with detailed movie information and layout

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FaPlay } from "react-icons/fa6";
 
 const MovieDetail = () => {
   return (
-    <div>MovieDetail</div>
-  )
-}
+    <div>
+      <img src="https://image.tmdb.org/t/p/w500/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg" />
+      <div>
+        <img src="https://image.tmdb.org/t/p/w500/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg" />
+        <div>
+          <p>Test</p>
+          <div>
+            <span>G</span>
+            <p>2024/11/11</p>
+            <p>Fantasy, Adventure</p>
+          </div>
+          <div>
+            <div>73 rating</div>
+            <button>
+              <FaPlay className="inline-block" /> Trailer
+            </button>
+          </div>
+          <div>
+            <p>Overview</p>
+            <p>Afte...</p>
+          </div>
+          <div>
+            <div>
+              <p>Director</p>
+              <p>Jennifer Phang</p>
+            </div>
+            <div>
+              <p>Writer</p>
+              <p>Jennifer Phang</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;

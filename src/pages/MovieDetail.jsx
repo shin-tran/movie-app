9d83438517816252fb1f88b161f7ba3787464db3
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-05-02 10:57:52 +0700

    refactor: improve layout structure and styling in MovieDetail component

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -8,14 +8,14 @@ const MovieDetail = () => {
         className="absolute inset-0 brightness-[0.2]"
         src="https://image.tmdb.org/t/p/original/65MVgDa6YjSdqzh7YOA04mYkioo.jpg"
       />
-      <div className="flex relative max-w-xl mx-auto p-6">
+      <div className="relative mx-auto flex max-w-xl gap-6 p-6">
         <div className="flex-1">
           <img src="https://image.tmdb.org/t/p/w500/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg" />
         </div>
-        <div className="flex-[2]">
-          <p>Test</p>
-          <div>
-            <span>G</span>
+        <div className="flex-[2] text-[1.2vw]">
+          <p className="mb-2 text-[2vw] font-bold">Test</p>
+          <div className="flex gap-4 items-center">
+            <span className="border border-gray-400 p-1 text-gray-400">G</span>
             <p>2024/11/11</p>
             <p>Fantasy, Adventure</p>
           </div>
